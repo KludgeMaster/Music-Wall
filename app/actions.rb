cc6536fcca5e74d2d1ed9011ebb3a131b76f5ab7
KludgeMaster <yiryans@gmai.com>
2015-08-10 22:08:10 +0000

    working

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,29 @@
 # Homepage (Root path)
 get '/' do
+  @songs = Song.all
   erb :index
 end
+
+get '/new' do
+  erb :new
+end
+
+get '/index' do
+  @songs = Song.all
+  erb :index
+end
+
+post '/new' do
+  @song = Song.new(
+    title:  params[:title],
+    author: params[:author],
+    url:    params[:url]
+  )
+  if @song.save
+    redirect '/index'
+  else
+    erb :new
+  end
+end
+
+
